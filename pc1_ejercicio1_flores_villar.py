333f44cf0fdd35b81629844d58002642e1f5c404
FloresVillar <efloresv@uni.pe>
2025-04-13 17:43:44 -0500

    aa

--- pc1_ejercicio1_flores_villar.py
+++ pc1_ejercicio1_flores_villar.py
@@ -1,24 +1,37 @@
 import sys
 from collections import deque;
-from utils import *
+
 #librerias necesariasa
-class Problem:
+class Problema:
 	def __init__(self,inicial,final=None):
 		self.inicial = inicial
 		self.final = final
 	def is_goal(self,estado):
 		if isinstance(self.final,list):
-			return is_in(estado,self.fin)
+			return is_in(estado,self.final)
 		else:
 			return estado==self.final
 	def accciones(self,estado):
 		raise NotImplementedError #las acciones para el problema particular
 	def resultado(self,estado,accion):
 		raise NotImplementedError #los resultados al ejecutar la accion a ese estado
-	def costo_camino(self,c_hasta1,estado1,accion,estado2):
+	def costo_accion(self,c_hasta1,estado1,accion,estado2):
 		return c_hasta1 + 1
 	def valor(self,estado):
 		raise NotImplementedError
+
+class Descifrado(Problema):
+	def __init__(self,texto_cifrado,fragmento_conocido,diccionario,inicial = None):
+		super().__init__(inicial)
+		self.texto_cifrado = texto_cifrado #Jvsq, Mti, Xlmz.
+		self.fragmento_conocido = fragmento_conocido # 'hola' aparece en el texto
+		self.diccionario = diccionario #['Hola', 'Mundo', 'Cifrado', 'Texto']
+	def is_goal(self,mapeo):
+		texto_descifrado = self.aplicar_mapeo(mapeo)
+		if self.fragmento_conocido in texto_descifrado:
+			return True
+		return False
+
 class Nodo:
 	def __init__(self,estado,padre =None,accion =None,costo_camino =0):
 		self.estado = estado
@@ -32,29 +45,29 @@ class Nodo:
 		return f"<nodo {self.estado}"
 	def __lt__(self,nodo):
 		return self.estado < nodo.estado
-	def expandir(self, problema)
+	def expandir(self, problema):
 		lista = []
 		for accion in problema.acciones(self.estado):
 			hijo = self.nodo_hijo(problema,accion)
 			lista.append(hijo)
 		return lista
 	def nodo_hijo(self,problema,accion):
-		siguiente_estado = problema.resultado(self.estado,accion):
-		siguiente_nodo = Nodo(siguiente_estado,self,accion,problema.costo_camino(self.costo_camino,self.estado,accion,siguiente_estado))
+		siguiente_estado = problema.resultado(self.estado,accion)
+		siguiente_nodo = Nodo(siguiente_estado,self,accion,problema.costo_accion(self.costo_camino,self.estado,accion,siguiente_estado))
 		return siguiente_nodo
 	def solucion(self):
 		acciones = []
 		camino = self.camino()[1:]
 		for nodo in camino:
-			acciones.append(node.accion)
+			acciones.append(nodo.accion)
 		return acciones
-	def camino():
+	def camino(self):
 		nodo, camino_atras = self, []
 		while nodo:
 			camino_atras.append(nodo)
 			nodo = nodo.padre
 		return list(reversed(camino_atras))
 	def __eq__(self,otro):
-		return isinstance() and self.estado == otro.estado
+		return isinstance(otro,Nodo) and self.estado == otro.estado
 	def __hash__(self):
-		
+		return hash(self.estado) #n1=Nodo("A") n2=Nodo("A") conjunto=set() conjunto.add(n1) n2 in conjunto ===> true
